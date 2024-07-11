9aaaab5896c64cb534fdd539b6a441d643b9f7f6
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-11 20:21:19 +0600

    Implemented sample delegate and methods for testing communication

--- PJSIPFramework/PJSIPLibrary.swift
+++ PJSIPFramework/PJSIPLibrary.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 
-public class PJSIPLibrary: NSObject {
+public protocol PJSIPLibraryDelegate: AnyObject {
+    func incomingCallReceived(callId: String)
+    
+    func acccountUpdateReceived(status: Bool)
+    
+    func callStatusRecieved(status: Int)
+}
+
+public class PJSIPLibrary {
+    
+    public init() {
+        print("Initialized")
+//        CPPWrapper().createLibWrapper()
+    }
+    
+    // Delegate property
+    public weak var delegate: PJSIPLibraryDelegate?
+    
     public func x() {
         print("Hello X")
+        delegate?.incomingCallReceived(callId: "OLOPOLO")
+    }
+    
+    public func y() -> String {
+        return "Sdasda"
     }
 }
