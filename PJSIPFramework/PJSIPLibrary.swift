6dd54d59ca8b1a0532a847419406bd5fcdc97d5b
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-07-11 18:12:05 +0600

    Added nececcary files and configured the files

---
+++ PJSIPFramework/PJSIPLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  PJSIPLibrary.swift
+//
+//
+//  Created by Shahwat Hasnaine on 7/11/24.
+//
+
+import Foundation
+
+public class PJSIPLibrary: NSObject {
+    public func x() {
+        print("Hello X")
+    }
+}
